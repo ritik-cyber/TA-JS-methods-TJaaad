004a9877d708fabd127f2eb7e70e3cea236add63
ritik-cyber <ritikdhingra03@gmail.com>
2021-06-09 14:17:39 +0530

    done agaon

--- block-BJaadl/code/index.js
+++ block-BJaadl/code/index.js
@@ -16,37 +16,44 @@ function findLongestWord(words) {
 let newWords = [...words].sort((a, b) => a.length - b.length).pop();
 console.log(newWords);
 // - Convert the above array "words" into an array of length of word instead of word.
-
+let wordsLength = words.map((w) => w.length);
+console.log(wordsLength);
 // - Create a new array that only contains word with atleast one vowel.
-
-// function oneVovel(words) {
-//   return words;
-// }
-// let oneVovel1 = words.filter((element) => element);
+function checkVovel(words) {
+  return (
+    words.toLowerCase().includes("a") ||
+    words.toLowerCase().includes("e") ||
+    words.toLowerCase().includes("i") ||
+    words.toLowerCase().includes("o") ||
+    words.toLowerCase().includes("u")
+  );
+}
+let wordsVovel = words.filter((w) => checkVovel(w));
+console.log(wordsVovel);
 
 // - Find the index of the word "rhythm"
 console.log(words.indexOf("rhythm"));
 
 // - Create a new array that contians words not starting with vowel.
-// let startWith = words.filter((words) =>
-//   words.startsWith("a", "e", "i", "o", "u")
-// );
-// console.log(startWith);
+let wordsVovelNot = words.filter((w) => !checkVovel(w[0]));
+console.log(wordsVovelNot);
 // - Create a new array that contianse words not ending with vowel
+let wordsVovelEnd = words.filter((w) => !checkVovel(w[w.length - 1]));
+console.log(wordsVovelEnd);
 // let endWith = words.filter((words) => words.endWith("a", "e", "i", "o", "u"));
 // console.log(endWith);
 
 let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 // - Create a sumArray function that takes an array of number as a parameter, and calculate the sum of all its num
-// function sumArray(numbers) {
-//   let sum = 0;
-//   for (let i = 0; i < numbers.length; i++) {
-//     sum += numbers;
-//   }
-//   return sum;
-// }
-// console.log(sum);
+function sumArray(array) {
+  return array.reduce((acc, cv) => {
+    acc = acc + cv;
+    return acc;
+  }, 0);
+}
+console.log(sumArray(numbers));
+console.log(sum);
 // - Make a new array that contains number multiplied by 3 like [6, 18, 27 ...]
 function multipliedBy3(num) {
   return num % 3 === 0;
@@ -66,9 +73,11 @@ function odd(num) {
 let oddNumber = numbers.filter(odd);
 console.log(oddNumber);
 // - Create a new array that should have true for even number and false for odd numbers.
-
+let oddOrEven = numbers.map((num) => num % 2 === 0);
+console.log(oddOrEven);
 // - Sort the above number in assending order.
-console.log([...numbers].sort((a, b) => a.length - b.length).pop());
+let sortNumber = [...numbers].sort((a, b) => a - b);
+console.log(sortNumber);
 // - Does sort mutate the original array?
 console.log(numbers); // yes
 // - Find the sum of the numbers in the array.
@@ -97,7 +106,14 @@ let strings = [
 
 // - Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
 
-function averageWordLength(strings) {
-  return strings.length;
+function averageWordLength(words) {
+  return (
+    words
+      .map((w) => w.length)
+      .reduce((acc, cv) => {
+        acc = acc + cv;
+        return acc;
+      }, 0) / words.length
+  );
 }
-console.log(averageWordLength());
+console.log(averageWordLength(strings));
